e359a391d87f63ab1442d6f0c54b304cab899dac
sagheer ahmad <G00357770@gmit.ie>
2020-05-17 16:34:37 +0000

    Removed a few files

--- projectCommandLine.py
+++ projectCommandLine.py
@@ -1,8 +1,10 @@
+#import the packages and class
 import argparse
 import project
 
 #add the argument parser
 parser = argparse.ArgumentParser(add_help=False)
+#add the arguments to the parser
 parser.add_argument("project", help="Graph Theory Project")
 parser.add_argument("--instructions", help="In order to run this program, please use the following command 'python3 project.py'. Then follow the instructions on the screen to test your regular expression")
 parser.add_argument("--infix", required=True, help="This is the infix that will be compared to the string",type=str)
@@ -11,4 +13,5 @@ parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, he
 
 args = parser.parse_args()
 
+#run the match function from the project class
 print(" Result:", project.match(str(args.nfa), str(args.string)))
