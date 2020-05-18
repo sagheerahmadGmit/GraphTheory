cf4b3c0f5c9bfbc3ced8dfd200b892012f91d982
sagheer ahmad <G00357770@gmit.ie>
2020-05-18 15:03:59 +0000

    Fixed error in command line arguments

--- projectCommandLine.py
+++ projectCommandLine.py
@@ -5,7 +5,6 @@ import project
 #add the argument parser
 parser = argparse.ArgumentParser(add_help=False)
 #add the arguments to the parser
-parser.add_argument("project", help="Graph Theory Project")
 parser.add_argument("--instructions", help="In order to run this program, please use the following command 'python3 project.py'. Then follow the instructions on the screen to test your regular expression")
 parser.add_argument("--infix", required=True, help="This is the infix that will be compared to the string",type=str)
 parser.add_argument("--string", required=True, help="This is the string that will be matched to the infix",type=str)
@@ -14,4 +13,4 @@ parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, he
 args = parser.parse_args()
 
 #run the match function from the project class
-print(" Result:", project.match(str(args.nfa), str(args.string)))
+print(" Result:", project.match(str(args.infix), str(args.string)))
